097458ac5506a06b870ca6aa7878b3b61c7f8b0e
lukassen <lukassen@a124bc62-7122-0410-9567-f71c85e761d6>
2007-01-24 14:12:37 +0000

    prepared LLLog allows to disable logging

---
+++ Classes/Luky/LLSystem/LLLog.h
@@ -0,0 +1,11 @@
+//
+//  LLLog.h
+//  MacTrek
+//
+//  Created by Chris & Judith Lukassen on 24/01/2007.
+//  Copyright 2006 Luky Soft. LGPL Licence.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define LLLog(format, args...) NSLog(format, args) 
