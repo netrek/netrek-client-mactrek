d2d531255d4163e8d629016debe5c8f4ed8a9076
lukassen <lukassen@a124bc62-7122-0410-9567-f71c85e761d6>
2007-03-16 18:01:59 +0000

    new entry screen and bugfix in postman to allow players up to k to send messges

--- trunk/Classes/MacTrek/Gui/Postman.m
+++ trunk/Classes/MacTrek/Gui/Postman.m
@@ -209,7 +209,7 @@
         char playerId = [address characterAtIndex:1];
         if (playerId >= '0' && playerId <= '9') {
             playerId -= '0';
-        } else if (playerId >= 'a' && playerId <= 'f'){
+        } else if (playerId >= 'a' && playerId <= 'k'){ // WAS f but i see more players !!!!!
             playerId -= 'a';
             playerId += 10;
         } else {
