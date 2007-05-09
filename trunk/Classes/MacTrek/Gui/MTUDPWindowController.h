ffc4e477432adbd72b4051d882d7bda0739c6320
lukassen <lukassen@a124bc62-7122-0410-9567-f71c85e761d6>
2007-05-09 21:07:05 +0000

    added UDP control panel

---
+++ trunk/Classes/MacTrek/Gui/MTUDPWindowController.h
@@ -0,0 +1,25 @@
+/* MTUDPWindowController */
+
+#import <Cocoa/Cocoa.h>
+
+@interface MTUDPWindowController : NSObject
+{
+    IBOutlet NSSegmentedControl *channelState;
+    IBOutlet NSComboBox *receiveModeBox;
+    IBOutlet NSButton *resetButton;
+    IBOutlet NSComboBox *sendModeBox;
+    IBOutlet NSSegmentedControl *sequenceChecking;
+    IBOutlet NSTextField *statisticsField;
+    IBOutlet NSTextField *statusField;
+    IBOutlet NSPanel *udpPanel;
+    IBOutlet NSButton *updateAllButton;
+}
+- (IBAction)resetToTcp:(id)sender;
+- (IBAction)updateAll:(id)sender;
+
+- (void)channelStateChanged:(NSSegmentedControl*)sender;
+- (void)sequenceChecking:(NSSegmentedControl*)sender;
+- (void)sendModeChanged:(NSComboBox*)sender;
+- (void)receiveModeChanged:(NSComboBox*)sender;
+
+@end
