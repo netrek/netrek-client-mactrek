216ebbbed478fcbec48892faa4afa14a3f2073f3
lukassen <lukassen@a124bc62-7122-0410-9567-f71c85e761d6>
2010-01-18 14:50:39 +0000

    started porting to 10.6, fixing depreciation stuff

--- trunk/Classes/Luky/LLGui/LLScreenResolution.h
+++ trunk/Classes/Luky/LLGui/LLScreenResolution.h
@@ -8,18 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 #import "LLObject.h"
-//#import <CoreGraphics/CoreGraphics.h>
-
-/* based on ModeWhacker.c */
-#include <ApplicationServices/ApplicationServices.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-/*
- * Arbitrary value.  For purposes of this example I don't expect
- * more than this many on a typical desktop system...
- */
-#define kMaxDisplays  16
 
 // use this to define mode e.g.
 //
@@ -44,26 +32,5 @@ struct screenMode {
 - (NSRect) frameForPrimairyDisplay;
 - (struct screenMode) screenModeOnPrimairyDisplay;
 - (void) setPrimairyDisplayToMode: (struct screenMode) mode;
-
-/*
-// internal??
-- (bool) supportForAquaOnPrimairyDisplay;
-- (int)  displayWidthOnPrimairyDisplay;
-- (int)  displayHeigthOnPrimairyDisplay;
-- (int)  displayBitsPerPixelOnPrimairyDisplay;
-- (int)  displayRefreshRateOnPrimairyDisplay;
-
-// internal
-- (CFDictionaryRef) descriptionForPrimairyDisplay;
-- (CGDirectDisplayID) primairyDisplayId;
-- (void) setDisplay:(CGDirectDisplayID) dspy toMode: (struct screenMode) screenMode;
-- (int) numberOfDisplays;
-- (NSArray *) arrayOfDisplayIDs;
-- (bool) supportForAquaInDescr: (CFDictionaryRef) desc;
-- (int) displayWidthInDescr: (CFDictionaryRef) desc;
-- (int) displayHeigthInDescr: (CFDictionaryRef) desc;
-- (int) displayBitsPerPixelInDescr: (CFDictionaryRef) desc;
-- (int) displayRefreshRateInDescr: (CFDictionaryRef) desc;
-*/
 
 @end
